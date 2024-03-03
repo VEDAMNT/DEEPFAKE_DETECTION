70bace0d6dbacaad78d79a0da80c51348accf90e
VEDAMNT <99802920+VEDAMNT@users.noreply.github.com>
2024-03-03 20:47:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ from keras.models import load_model
 model = load_model('Model\Model10Epochs30batch.h5')
 FilePathTest = ""
 ImageToAnalys = ""
+
+def OpenFilePathTest():
+    global FilePathTest 
+    FilePathTest = filedialog.askopenfilename()
+    print(FilePathTest)
+    if FilePathTest != "":
+        UpdateImage(FilePathTest)
